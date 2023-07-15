12222d77da95640e25a325129ca0a480777bc075
Péter Ott <ottpeter@tuta.io>
2023-07-15 12:25:57 -0400

    Adds constants

--- contract/contracts/VotingApp.sol
+++ contract/contracts/VotingApp.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.8.9;
 
 
 contract VotingApp {
-  
+    uint16 constant MAX_NAME_LENGTH = 32;
+    uint16 constant MAX_DESC_LENGTH = 256;
+    uint16 constant MAX_OPTION_LENGTH = 128;
+    uint16 constant MAX_OPTION_NUM = 16;
 }
