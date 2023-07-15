250aa9366f3a184c08ffa8a6d55d4774b9aff394
Péter Ott <ottpeter@tuta.io>
2023-07-15 13:43:16 -0400

    Adds createPoll()

--- contract/contracts/VotingApp.sol
+++ contract/contracts/VotingApp.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract VotingApp {
-    uint16 constant MAX_NAME_LENGTH = 32;
-    uint16 constant MAX_DESC_LENGTH = 256;
+    uint16 constant MAX_NAME_LENGTH = 32;                               // These will be bytes,
+    uint16 constant MAX_DESC_LENGTH = 256;                              // because when we are using it we are doing bytes(value).length
     uint16 constant MAX_OPTION_LENGTH = 128;
     uint16 constant MAX_OPTION_NUM = 16;
 
@@ -17,7 +17,6 @@ contract VotingApp {
     struct Poll {
         string name;                                                    // Name of the poll
         string description;                                             // Description of the poll
-        Counters.Counter totalVoters;                                   // Total number of voters
         uint256 createdTime;                                            // block.timestamp (UNIX timestamp)
         uint256 endTime;                                                // UNIX timestamp received from the front end
         Counters.Counter totalVoteCount;                                // Total votes received on this poll
@@ -39,6 +38,60 @@ contract VotingApp {
 
     constructor() {
         pollNonce = Counters.Counter(0);
+    }
+
+
+    /// This will create a new Poll object, based on data that will come from the front end
+    /// Returns the poll ID
+    /// @param name Name of the poll
+    /// @param desc Description for the poll
+    /// @param end poll expire date, UNIX timestamp
+    /// @param options String array of option names
+    function createPoll(
+        string calldata name,
+        string calldata desc,
+        uint256 end,
+        string[] calldata options
+    ) public payable returns(uint256) {
+        require(bytes(name).length < MAX_NAME_LENGTH, string(abi.encodePacked("The max length for the title is ", MAX_NAME_LENGTH)));
+        require(bytes(desc).length < MAX_DESC_LENGTH, string(abi.encodePacked("The max length for the description is ", MAX_DESC_LENGTH)));
+        require(end > block.timestamp, "Expiration date has to be bigger then current time!");
+        require(options.length < MAX_OPTION_NUM, string(abi.encodePacked("The max number of options ", MAX_OPTION_NUM)));
+
+        // We increment poll nonce. First ID will be 1
+        pollNonce.increment();
+
+
+        // Create the basic poll object
+        Poll memory newPoll = Poll(
+            name,
+            desc,
+            block.timestamp,
+            end,
+            Counters.Counter(0),
+            pollNonce.current()
+        );
+
+        // This will add the options to the associated optionsStorage array
+        for (uint16 i = 0; i < options.length; i++) {
+            require(
+                bytes(options[i]).length < MAX_OPTION_LENGTH,
+                string(abi.encodePacked("The max length for an option is ", MAX_OPTION_LENGTH))
+            );
+
+            Option memory tempOption = Option(
+                options[i],
+                Counters.Counter(0)
+            );
+            optionsStorage[pollNonce.current()].push(tempOption);
+        }
+
+        polls[pollNonce.current()] = newPoll;                           // Add the poll to the polls mapping
+        activePolls.push(pollNonce.current());                          // List the poll as active
+
+        removeExpired();                                                // This will cost some additional gas,
+                                                                        // but activePolls will be more up-to-date (can be removed)
+        return pollNonce.current();
     }
 
 
