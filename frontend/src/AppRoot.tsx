b3f66e1255841b9415e1a865f14910e57ba1bc2b
Péter Ott <ottpeter@tuta.io>
2023-07-18 13:27:31 -0400

    Adds feature to automatically switch network if user is on the wrong network

--- frontend/src/AppRoot.tsx
+++ frontend/src/AppRoot.tsx
@@ -3,6 +3,8 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from "react";
+declare let window: any;
 
 
 /** 
@@ -10,6 +12,34 @@ import 'react-toastify/dist/ReactToastify.css';
  * (this is what React Router considers root)
  * */
 export default function AppRoot() {
+
+  // Switch network if we are not on Sepoliass
+  useEffect(() => {
+    const switchToSepolia = async () => {
+      const networkId = "0xaa36a7";   // Sepolia
+      if (window.ethereum) {
+        if (window.ethereum.networkVersion != 11155111) {
+          try {
+            await window.ethereum.request({
+              method: "wallet_switchEthereumChain",
+              params: [{ chainId: networkId }],
+            });
+            window.location.reload();
+          } catch (error) {
+            console.error("Error switching network:", error);
+            toast.error("Error switching network. This app only works on the Sepolia test network.");
+          }
+        }
+      } else {
+        toast.error("Can not connect to MetaMask");
+      } 
+    }
+
+    switchToSepolia();
+  }, []);
+
+  
+
   return(
     <>
       <Nav />
