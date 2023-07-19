a233f0f1c72b938c1f864ad6e8a1fce04a4a66ab
Péter Ott <ottpeter@tuta.io>
2023-07-19 13:59:24 -0400

    Adds details to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
 
 export default function Footer() {
   return (
-    <footer>The Footer</footer>
+    <footer>
+      <p>Mady by Peter Ott</p>
+      <p>
+        <a href={"https://twitter.com/ottpeter2"} className="socialIcon" target="_blank"><FaTwitter size={22}/></a>
+        <a href={"https://www.linkedin.com/in/p%C3%A9ter-o-b4a20218a/"} className="socialIcon" target="_blank"><FaLinkedin size={22}/></a>
+        <a href={"https://github.com/ottpeter/"} className="socialIcon" target="_blank"><FaGithub size={22} /></a>
+      </p>
+        
+      </footer>
   )
 }
